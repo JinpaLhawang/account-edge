d7f8353d29352e2b85f34afa384046b487260ee2
Nicholas Eden-Walker <nedenwalker@kenzan.com>
2016-05-14 14:48:29 -0400

    Added ribbon client for account-middle.

--- src/main/java/com/jinpalhawang/jambudvipa/AccountEdgeRestController.java
+++ src/main/java/com/jinpalhawang/jambudvipa/AccountEdgeRestController.java
@@ -7,13 +7,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RefreshScope
+@RibbonClient(name = "account-middle", configuration = AccountMiddleConfiguration.class)
 public class AccountEdgeRestController {
 
   private static final Logger log = LoggerFactory.getLogger(AccountEdgeRestController.class);
@@ -23,6 +28,12 @@ public class AccountEdgeRestController {
 
   @Autowired
   private AccountService accountService;
+
+  @LoadBalanced
+  @Bean
+  RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
 
   @RequestMapping("/")
   public Account getAccount() {
--- src/main/java/com/jinpalhawang/jambudvipa/AccountService.java
+++ src/main/java/com/jinpalhawang/jambudvipa/AccountService.java
@@ -3,6 +3,7 @@ package com.jinpalhawang.jambudvipa;
 import java.net.URI;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
@@ -19,13 +20,13 @@ public class AccountService {
   @Value("${fallback.account.lastname}")
   private String fallbackAccountLastName;
 
+  @Autowired
+  RestTemplate restTemplate;
+
   @HystrixCommand(fallbackMethod = "fallback")
   public Account getAccount() {
-    final RestTemplate restTemplate = new RestTemplate();
-    // TODO: Replace with ribbon call
-    final URI uri = URI.create("http://localhost:8090/accounts/search/findByFirstName?firstName=Jinpa");
-
-    final Account account = restTemplate.getForObject(uri, Account.class);
+    final URI uri = URI.create("http://account-middle/accounts/search/findByFirstName?firstName=Jinpa");
+    final Account account = this.restTemplate.getForObject(uri, Account.class);
 
     return account;
   }
